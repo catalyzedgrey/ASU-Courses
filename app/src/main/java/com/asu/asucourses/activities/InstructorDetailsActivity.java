84a8d079ba13aa15903304dcd01090ce3772c8f9
Amr Hany <inertgrey@outlook.com>
2019-07-22 18:47:03 +0200

    Added instructor details activity

---
+++ app/src/main/java/com/asu/asucourses/activities/InstructorDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.asu.asucourses.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.asu.asucourses.R;
+
+public class InstructorDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructor_details);
+    }
+}
