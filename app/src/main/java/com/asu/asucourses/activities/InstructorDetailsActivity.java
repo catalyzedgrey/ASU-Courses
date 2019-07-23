d241032402527b680b36f99d6547f68c362e0d58
Grey <inertgrey@outlook.com>
2019-07-23 16:58:22 +0200

    Made minor modifications for the InstructorDetailsActivity

--- app/src/main/java/com/asu/asucourses/activities/InstructorDetailsActivity.java
+++ app/src/main/java/com/asu/asucourses/activities/InstructorDetailsActivity.java
@@ -2,14 +2,35 @@ package com.asu.asucourses.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.asu.asucourses.R;
 
 public class InstructorDetailsActivity extends AppCompatActivity {
 
+    ImageView imgView;
+    TextView instructorNameTV, instructorEmailTv, instructorBioTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructor_details);
+
+        initViews();
+        bindViews();
+    }
+
+    private void initViews(){
+        imgView = findViewById(R.id.instructor_img_view);
+        instructorNameTV = findViewById(R.id.instructor_tv);
+        instructorEmailTv = findViewById(R.id.email_tv);
+        instructorBioTV = findViewById(R.id.bio_tv);
+
+    }
+
+    private void bindViews(){
+        //get extras and bind them with the UI.
+
     }
 }
