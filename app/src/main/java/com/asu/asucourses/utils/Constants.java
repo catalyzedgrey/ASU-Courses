2ac16ccc1341f2e19f2576dd4f69cae40f253be2
Grey <inertgrey@outlook.com>
2019-07-23 16:58:47 +0200

    Added general classes to help with the service calls

---
+++ app/src/main/java/com/asu/asucourses/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.asu.asucourses.utils;
+
+public abstract class Constants {
+    public static final String trackUrl = "http://jsonstub.com/load-tracks";
+    public static final String coursesUrl = "http://jsonstub.com/load-courses";
+}
