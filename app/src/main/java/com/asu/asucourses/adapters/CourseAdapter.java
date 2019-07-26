1e4d4ac4d09dd5191dc534615589f96155dc9724
Osama Maged <eng.osamamaged@yahoo.com>
2019-07-26 18:22:18 +0200

    Created [CourseAdapter, CourseItem, activity_course_item] Modified [            OnItemClickListener, //to add onCourseClick            MainActivity // to add onCourseClick
             ]

---
+++ app/src/main/java/com/asu/asucourses/adapters/CourseAdapter.java
@@ -0,0 +1,60 @@
+package com.asu.asucourses.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.asu.asucourses.R;
+import com.asu.asucourses.models.Course;
+import com.asu.asucourses.itemviews.CourseItem;
+import com.asu.asucourses.models.OnItemClickListener;
+import com.asu.asucourses.models.Track;
+
+import java.util.List;
+
+public class CourseAdapter  extends RecyclerView.Adapter<CourseItem> implements OnItemClickListener {
+    private List<Course> courses;
+    private OnItemClickListener listener;
+
+    public CourseAdapter(List<Course> courses, OnItemClickListener listener) {
+        this.courses = courses;
+        this.listener = listener;
+    }
+
+    public CourseAdapter() {
+    }
+
+    @NonNull
+    @Override
+    public CourseItem onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_course_item, viewGroup, false);
+        return new CourseItem(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseItem courseItem, int i) {
+        courseItem.bindTrack(courses.get(i), listener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return courses == null ? 0 : courses.size();
+    }
+
+
+    public void refreshCourseAdapter(List<Course> courses){
+        this.courses = courses;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCourseClick(Course item) {
+       //open course details
+    }
+    @Override
+    public void onTrackClick(Track item) {
+        //open course details
+    }
+}
---
+++ app/src/main/java/com/asu/asucourses/itemviews/CourseItem.java
@@ -0,0 +1,32 @@
+package com.asu.asucourses.itemviews;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.asu.asucourses.R;
+import com.asu.asucourses.models.Course;
+import com.asu.asucourses.models.OnItemClickListener;
+
+
+public class CourseItem extends RecyclerView.ViewHolder {
+
+    private TextView title;
+
+
+    public CourseItem(@NonNull View itemView) {
+        super(itemView);
+        title = itemView.findViewById(R.id.itemViewCourse);
+    }
+
+    public void bindTrack(final Course course, final OnItemClickListener listener) {
+        title.setText(course.getCourseName());
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onCourseClick(course);
+            }
+        });
+    }
+}
