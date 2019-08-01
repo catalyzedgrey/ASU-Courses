fb99dcf0d22696033c98624da7348755d2f2a31b
Amr Hany <inertgrey@outlook.com>
2019-08-01 13:18:06 +0200

    Changes to the UI

--- app/src/main/java/com/asu/asucourses/activities/InstructorDetailsActivity.java
+++ app/src/main/java/com/asu/asucourses/activities/InstructorDetailsActivity.java
@@ -1,24 +1,37 @@
 package com.asu.asucourses.activities;
 
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 import com.asu.asucourses.R;
+import com.asu.asucourses.adapters.CourseAdapter;
+import com.asu.asucourses.interfaces.IService;
 import com.asu.asucourses.models.Instructor;
+import com.asu.asucourses.services.CoursesService;
 import com.asu.asucourses.utils.Constants;
 import com.bumptech.glide.Glide;
 
+import java.util.List;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class InstructorDetailsActivity extends AppCompatActivity {
+public class InstructorDetailsActivity extends AppCompatActivity implements IService {
 
     ImageView imgView;
-    TextView instructorNameTV, instructorEmailTv, instructorBioTV;
+    TextView instructorNameTV, instructorEmailTv, instructorBioTV, smallNameTV;
     Instructor instructor;
-    CircleImageView circleImageView;
+    CircleImageView circleImageView, smallCircleImage;
 
+    RecyclerView recView;
+    CourseAdapter courseAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +42,23 @@ public class InstructorDetailsActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+
         //imgView = findViewById(R.id.instructor_img_view);
         instructorNameTV = findViewById(R.id.instructor_tv);
         instructorEmailTv = findViewById(R.id.email_tv);
         instructorBioTV = findViewById(R.id.bio_tv);
         circleImageView = findViewById(R.id.InstructorImage);
+        smallCircleImage = findViewById(R.id.small_inst_img);
+        smallNameTV = findViewById(R.id.small_inst_name);
+
+        recView = findViewById(R.id.recycler_view);
+        courseAdapter = new CourseAdapter();
+        recView.setLayoutManager(new LinearLayoutManager(recView.getContext()));
+        recView.setAdapter(courseAdapter);
+
+
+        new CoursesService(this).execute(Constants.coursesUrl);
+
 
     }
 
@@ -49,5 +74,10 @@ public class InstructorDetailsActivity extends AppCompatActivity {
         Glide.with(this).load(instructor.getInstructorImageUrl()).into(circleImageView);
         instructorBioTV.setText(instructor.getBio());
 
+    }
+
+    @Override
+    public void onTaskCompleted(List objects) {
+        courseAdapter.refreshCourseAdapter(objects);
     }
 }
